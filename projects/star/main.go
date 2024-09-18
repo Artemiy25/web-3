8fc82339bc4b9910c7efc4220505e7ec551d9287
Artemiy <artemiy.erhipov@gmail.com>
2024-09-18 20:07:41 +0300

    New code

--- projects/star/main.go
+++ projects/star/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var a, b string
+	fmt.Scan(&a)
+	for i := 0; i < len(a)-1; i++ {
+		b = b + string(a[i]) + "*"
+	}
+	b = b + string(a[len(a)-1])
+	fmt.Println(b)
 }
